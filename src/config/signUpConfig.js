33b7d9234fbf84fc057475a9971b9896cf12d164
yeeunKo <kiye5244@gmail.com>
2020-12-07 17:27:11 +0900

    testing for amplify

---
+++ src/config/signUpConfig.js
@@ -0,0 +1,24 @@
+const signUpConfig = {
+  header: 'Create an Amplify-Workshop Store Account',
+  hiddenDefaults: ['phone_number'],
+  defaultCountryCode: '1',
+  // signUpFields: [
+  //   {
+  //     label: 'First name',
+  //     key: 'given_name',
+  //     required: true,
+  //     displayOrder: 100,
+  //     type: 'string'
+  //   },
+  //   {
+  //     label: 'Country',
+  //     key: 'country',
+  //     required: true,
+  //     displayOrder: 106,
+  //     type: 'string',
+  //     custom: true
+  //   }
+  // ]
+};
+
+export default signUpConfig
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +16,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
